453cb7e640d2adc54d1f372251573cf93487d3e9
takumi-shibata <241448ta@gmail.com>
2020-06-29 13:46:04 +0900

    １周目学習終了

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ class Animal {
 }
 
 
-// インスタンスの生成(クラスから生成したオブジェクトをインスタンスと呼ぶ)
+// インスタンスの生成「new クラス名();」(クラスから生成したオブジェクトをインスタンスと呼ぶ)
 class Animal {
 }
 // Animalクラスのインスタンスを定数animalに代入してください
@@ -165,11 +165,193 @@ animal.info();
 // →3歳です
 
 
+// クラスの継承「extends」(新しく作成するクラスが既存のクラスの一種である場合「継承」という方法を用いる)
+// 例えば「Animalクラス」から「Dogクラス」を継承すると、「Animalクラス」の全ての機能を引き継いで、「Dogクラス」を作成することができる
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+// Animalクラスを継承してDogクラスを定義してください
+class Dog extends Animal {  
+}
+const animal = new Animal("レオ", 3);
+animal.info();
 
 
+// 継承したクラスの使用
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+class Dog extends Animal {
+}
+// 定数dogにDogクラスのインスタンスを代入してください
+const dog = new Dog("レオ",4);
+// dogに対してinfoメソッドを呼び出してください
+dog.info();
+// →こんにちは
+// →名前はレオです
+// →4歳です
 
 
+// メソッドの追加
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+class Dog extends Animal {
+  // getHumanAgeメソッドを追加してください
+  // メソッドでは戻り値を用いることができる
+  getHumanAge() {
+    return this.age * 7;
+  }
+}
+
+const dog = new Dog("レオ", 4);
+dog.info();
+
+// 定数humanAgeを定義し、定数dogに対してgetHumanAgeメソッドを呼び出した値を代入してください
+const humanAge = dog.getHumanAge();
+
+// 「人間年齢で〇〇歳です」と出力してください
+console.log("人間年齢で" + humanAge + "歳です");
+// →こんにちは
+// →名前はレオです
+// →4歳です
+// →人間年齢で28歳です
 
 
+// オーバーライド(親クラスと同じ名前のメソッドを子クラスに定義すると、子クラスのメソッドが優先して使用される)
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+class Dog extends Animal {
+  // infoメソッドを追加してください
+  // オーバーライド(子クラスのメソッドが優先して使用される)
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+    const humanAge = this.getHumanAge();
+    console.log(`人間年齢で${humanAge}歳です`);
+  }  
+
+  getHumanAge() {
+    return this.age * 7;
+  }
+}
+
+const dog = new Dog("レオ", 4);
+dog.info();
+// →こんにちは
+// →名前はレオです
+// →4歳です
+// →人間年齢で28歳です
 
 
+// コンストラクタのオーバーライド「super()」
+// ※親クラスのコンストラクタが引数を受け取る場合には、「super」の後ろの丸括弧「( )」に親クラスの引数を渡す必要がある！
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  greet() {
+    console.log("こんにちは");
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    console.log(`${this.age}歳です`);
+  }
+}
+
+class Dog extends Animal {
+  // コンストラクタのオーバーライド
+  // constructorを追加してください
+  constructor(name, age, breed) {
+  	// superに親クラスの引数を渡す!
+    super(name, age);
+    // 犬種の定義
+    this.breed = breed;
+  }
+  
+  info() {
+    this.greet();
+    console.log(`名前は${this.name}です`);
+    // 「犬種は〇〇です」と出力してください
+    console.log("犬種は" + this.breed + "です" );
+    
+    console.log(`${this.age}歳です`);
+    const humanAge = this.getHumanAge();
+    console.log(`人間年齢で${humanAge}歳です`);
+  }
+  
+  getHumanAge() {
+    return this.age * 7;
+  }
+}
+
+// 3つ目の引数に「"チワワ"」を渡してください
+const dog = new Dog("レオ", 4 ,"チワワ");
+dog.info();
+// →こんにちは
+// →名前はレオです
+// →犬種はチワワです
+// →4歳です
+// →人間年齢で28歳です
